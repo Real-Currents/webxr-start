f197a4b50f57b2eda2a3defb1f9d903143563c4a
John <john@real-currents.com>
2025-03-17 07:14:49 -0400

    move clipping plane logic to seperate function

--- src/main.js
+++ src/main.js
@@ -17,6 +17,101 @@ let currentSession;
 
 let waiting_for_confirmation = false;
 
+function getPortalClippingPlanes (renderer, camera) {
+
+    // Setup Clipping planes
+    let xrCamera = renderer.xr.getCamera(camera);
+    let xrCameraMatrix = xrCamera.matrixWorld;
+    let xrCameraA = new THREE.Vector3();
+    xrCameraA.setFromMatrixPosition(xrCameraMatrix);
+    // // xrCameraDirection From:
+    // // https://stackoverflow.com/questions/59554505/how-can-i-get-camera-world-direction-with-webxr#answer-59687055
+    // let xrCameraDX = xrCameraMatrix.elements[8],
+    //     xrCameraDY = xrCameraMatrix.elements[9],
+    //     xrCameraDZ = xrCameraMatrix.elements[10];
+    // let xrCameraDirection = new THREE.Vector3(-xrCameraDX, -xrCameraDY, -xrCameraDZ).normalize();
+
+    // console.log(xrCameraA);
+
+    const viewingPlaneLeft = -1; // x left
+    const viewingPlaneRight = 1; // x right
+    const viewingPlaneTop = 2.5; // y top
+    const viewingPlaneBottom = 1; // y bottom
+    const viewingPlaneDepth = 0; // z
+    const viewingPlaneHorizonalCenter = 0;
+    const viewingPlaneVerticalCenter= (viewingPlaneBottom + viewingPlaneTop) / 2; // (viewingPlaneTop - viewingPlaneBottom)/ 2 + viewingPlaneBottom
+
+    const clippingLeftP = new THREE.Vector3(viewingPlaneLeft, xrCameraA.y, viewingPlaneDepth);
+    const clippingRightP = new THREE.Vector3(viewingPlaneRight, xrCameraA.y, viewingPlaneDepth);
+    const clippingTopP = new THREE.Vector3(xrCameraA.x, viewingPlaneTop, viewingPlaneDepth);
+    const clippingBottomP = new THREE.Vector3(xrCameraA.x, viewingPlaneBottom, viewingPlaneDepth);
+
+    const vDLeft = new THREE.Vector3();
+    vDLeft.subVectors(clippingLeftP, xrCameraA);
+    const vDRight = new THREE.Vector3();
+    vDRight.subVectors(clippingRightP, xrCameraA);
+    const vDTop = new THREE.Vector3();
+    vDTop.subVectors(clippingTopP, xrCameraA);
+    const vDBottom = new THREE.Vector3();
+    vDBottom.subVectors(clippingBottomP, xrCameraA);
+
+    const clippingLeftUnitVector = new THREE.Vector3(1.0, 0, 0);
+    const clippingLeftDirection = vDLeft.clone().cross(new THREE.Vector3(0, 1.0, 0)).normalize();
+    const clippingLeftUnitAngleToDirection = clippingLeftUnitVector.angleTo(clippingLeftDirection.clone());
+    const clippingLeftX = Math.cos(clippingLeftUnitAngleToDirection) * viewingPlaneLeft;
+    const clippingRightUnitVector = new THREE.Vector3(-1.0, 0, 0);
+    const clippingRightDirection = vDRight.clone().cross(new THREE.Vector3(0, -1.0, 0)).normalize();
+    const clippingRightUnitAngleToDirection = clippingRightUnitVector.angleTo(clippingRightDirection.clone());
+    const clippingRightX = Math.cos(clippingRightUnitAngleToDirection) * viewingPlaneRight;
+    const clippingTopUnitVector = new THREE.Vector3(0, -1.0, 0);
+    const clippingTopDirection = vDTop.clone().cross(new THREE.Vector3(1.0, 0, 0)).normalize();
+    const clippingTopUnitAngleToDirection = clippingTopUnitVector.angleTo(clippingTopDirection.clone());
+    const clippingTopY = Math.cos(clippingTopUnitAngleToDirection) * viewingPlaneTop;
+    const clippingBottomUnitVector = (xrCameraA.z > viewingPlaneDepth) ?
+        new THREE.Vector3(0, 1.0, 0) :
+        new THREE.Vector3(0, -1.0, 0);
+    const clippingBottomDirection = vDBottom.clone().cross(new THREE.Vector3(-1, 0, 0)).normalize(); // new THREE.Vector3(0, 1, 0);
+    const clippingBottomUnitAngleToDirection = clippingBottomUnitVector.angleTo(clippingBottomDirection.clone());
+    const clippingBottomY = Math.cos(clippingBottomUnitAngleToDirection) * viewingPlaneBottom; // viewingPlaneBottom = 1.0 // length on unit circle
+
+    const clippingLeftPlane = new THREE.Plane(clippingLeftDirection.clone(), -clippingLeftX);
+    const clippingRightPlane = new THREE.Plane(clippingRightDirection.clone(), clippingRightX);
+    const clippingTopPlane = new THREE.Plane(clippingTopDirection.clone(), clippingTopY);
+    const clippingBottomPlane = new THREE.Plane(clippingBottomDirection.clone(), clippingBottomY * -(viewingPlaneBottom - 0.001));
+    const clippingPlaneInside = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+    const clippingPlaneOutside = new THREE.Plane(new THREE.Vector3(0, 0, -1), 0);
+
+    const clipping_data = {
+        "leftΘ": clippingLeftUnitAngleToDirection,
+        "leftX": clippingLeftX,
+        "rightΘ": clippingRightUnitAngleToDirection,
+        "rightX": clippingRightX,
+        "topΘ": clippingTopUnitAngleToDirection,
+        "topY": clippingTopY,
+        "bottomΘ": clippingBottomUnitAngleToDirection,
+        "bottomY": clippingBottomY
+    };
+
+    const clipping_data_for_html = JSON.stringify(clipping_data)
+        .replace(new RegExp("\\\\n", "g"), '<br />')
+        .replace(new RegExp('"\:', "g"), '":<br />')
+        .replace(new RegExp(',', "g"), '",<br />')
+        .replace(new RegExp("{", "g"), '{<br />')
+        .replace(new RegExp("}", "g"), '<br />}');
+
+    // console.log(clipping_data_for_html);
+
+    return [
+        clippingLeftPlane,
+        clippingRightPlane,
+        clippingTopPlane,
+        clippingBottomPlane,
+        ...(xrCameraA.z > viewingPlaneDepth) ?
+            [ clippingPlaneOutside ] :
+            [ clippingPlaneInside, clippingPlaneOutside ]
+    ];
+}
+
 async function initScene (setup = (scene, camera, controllers, players) => {}) {
 
     const clock = new THREE.Clock();
@@ -103,7 +198,7 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
         0.1,
         1000,
     );
-    camera.position.set(0, 1.6, 3);
+    camera.position.set(0, 1.6, 1);
 
     const controls = new OrbitControls(camera, container);
     controls.target.set(0, 1.6, 0);
@@ -333,97 +428,10 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
 
                 stats.begin();
 
-                // Setup Clipping planes
-                let xrCamera = renderer.xr.getCamera(camera);
-                let xrCameraMatrix = xrCamera.matrixWorld;
-                let xrCameraA = new THREE.Vector3();
-                xrCameraA.setFromMatrixPosition(xrCameraMatrix);
-                // // xrCameraDirection From:
-                // // https://stackoverflow.com/questions/59554505/how-can-i-get-camera-world-direction-with-webxr#answer-59687055
-                // let xrCameraDX = xrCameraMatrix.elements[8],
-                //     xrCameraDY = xrCameraMatrix.elements[9],
-                //     xrCameraDZ = xrCameraMatrix.elements[10];
-                // let xrCameraDirection = new THREE.Vector3(-xrCameraDX, -xrCameraDY, -xrCameraDZ).normalize();
-
-                // console.log(xrCameraA);
-
-                const viewingPlaneLeft = -1; // x left
-                const viewingPlaneRight = 1; // x right
-                const viewingPlaneTop = 2.5; // y top
-                const viewingPlaneBottom = 1; // y bottom
-                const viewingPlaneDepth = 0; // z
-                const viewingPlaneHorizonalCenter = 0;
-                const viewingPlaneVerticalCenter= (viewingPlaneBottom + viewingPlaneTop) / 2; // (viewingPlaneTop - viewingPlaneBottom)/ 2 + viewingPlaneBottom
-
-                const clippingLeftP = new THREE.Vector3(viewingPlaneLeft, xrCameraA.y, viewingPlaneDepth);
-                const clippingRightP = new THREE.Vector3(viewingPlaneRight, xrCameraA.y, viewingPlaneDepth);
-                const clippingTopP = new THREE.Vector3(xrCameraA.x, viewingPlaneTop, viewingPlaneDepth);
-                const clippingBottomP = new THREE.Vector3(xrCameraA.x, viewingPlaneBottom, viewingPlaneDepth);
-
-                const vDLeft = new THREE.Vector3();
-                vDLeft.subVectors(clippingLeftP, xrCameraA);
-                const vDRight = new THREE.Vector3();
-                vDRight.subVectors(clippingRightP, xrCameraA);
-                const vDTop = new THREE.Vector3();
-                vDTop.subVectors(clippingTopP, xrCameraA);
-                const vDBottom = new THREE.Vector3();
-                vDBottom.subVectors(clippingBottomP, xrCameraA);
-
-                const clippingLeftUnitVector = new THREE.Vector3(1.0, 0, 0);
-                const clippingLeftDirection = vDLeft.clone().cross(new THREE.Vector3(0, 1.0, 0)).normalize();
-                const clippingLeftUnitAngleToDirection = clippingLeftUnitVector.angleTo(clippingLeftDirection.clone());
-                const clippingLeftX = Math.cos(clippingLeftUnitAngleToDirection) * viewingPlaneLeft;
-                const clippingRightUnitVector = new THREE.Vector3(-1.0, 0, 0);
-                const clippingRightDirection = vDRight.clone().cross(new THREE.Vector3(0, -1.0, 0)).normalize();
-                const clippingRightUnitAngleToDirection = clippingRightUnitVector.angleTo(clippingRightDirection.clone());
-                const clippingRightX = Math.cos(clippingRightUnitAngleToDirection) * viewingPlaneRight;
-                const clippingTopUnitVector = new THREE.Vector3(0, -1.0, 0);
-                const clippingTopDirection = vDTop.clone().cross(new THREE.Vector3(1.0, 0, 0)).normalize();
-                const clippingTopUnitAngleToDirection = clippingTopUnitVector.angleTo(clippingTopDirection.clone());
-                const clippingTopY = Math.cos(clippingTopUnitAngleToDirection) * viewingPlaneTop;
-                const clippingBottomUnitVector = (xrCameraA.z > viewingPlaneDepth) ?
-                    new THREE.Vector3(0, 1.0, 0) :
-                    new THREE.Vector3(0, -1.0, 0);
-                const clippingBottomDirection = vDBottom.clone().cross(new THREE.Vector3(-1, 0, 0)).normalize(); // new THREE.Vector3(0, 1, 0);
-                const clippingBottomUnitAngleToDirection = clippingBottomUnitVector.angleTo(clippingBottomDirection.clone());
-                const clippingBottomY = Math.cos(clippingBottomUnitAngleToDirection) * viewingPlaneBottom; // viewingPlaneBottom = 1.0 // length on unit circle
-
-                const clippingLeftPlane = new THREE.Plane(clippingLeftDirection.clone(), -clippingLeftX);
-                const clippingRightPlane = new THREE.Plane(clippingRightDirection.clone(), clippingRightX);
-                const clippingTopPlane = new THREE.Plane(clippingTopDirection.clone(), clippingTopY);
-                const clippingBottomPlane = new THREE.Plane(clippingBottomDirection.clone(), clippingBottomY * -(viewingPlaneBottom - 0.001));
-                const clippingPlaneInside = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-                const clippingPlaneOutside = new THREE.Plane(new THREE.Vector3(0, 0, -1), 0);
-
-                const clipping_data = {
-                    "leftΘ": clippingLeftUnitAngleToDirection,
-                    "leftX": clippingLeftX,
-                    "rightΘ": clippingRightUnitAngleToDirection,
-                    "rightX": clippingRightX,
-                    "topΘ": clippingTopUnitAngleToDirection,
-                    "topY": clippingTopY,
-                    "bottomΘ": clippingBottomUnitAngleToDirection,
-                    "bottomY": clippingBottomY
-                };
-
-                const clipping_data_for_html = JSON.stringify(clipping_data)
-                    .replace(new RegExp("\\\\n", "g"), '<br />')
-                    .replace(new RegExp('"\:', "g"), '":<br />')
-                    .replace(new RegExp(',', "g"), '",<br />')
-                    .replace(new RegExp("{", "g"), '{<br />')
-                    .replace(new RegExp("}", "g"), '<br />}');
-
-                // console.log(clipping_data_for_html);
+                const clippingPlanes  = getPortalClippingPlanes(renderer, camera);
 
                 if (currentSession !== null) renderer.clippingPlanes =  [
-                    clippingLeftPlane,
-                    clippingRightPlane,
-                    clippingTopPlane,
-                    clippingBottomPlane,
-                    ...(xrCameraA.z > viewingPlaneDepth) ?
-                        [ clippingPlaneOutside ] :
-                        [ clippingPlaneInside, clippingPlaneOutside ]
-
+                    ...clippingPlanes
                 ];
 
                 updateScene(
@@ -433,11 +441,7 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
                     (Object.keys(sceneDataUpdate).length > 0) ? sceneDataUpdate : null,
                     null,
                     [
-                        clippingLeftPlane,
-                        clippingRightPlane,
-                        clippingTopPlane,
-                        clippingBottomPlane,
-                        clippingPlaneOutside
+                        ...clippingPlanes
                     ]
                 );
 
