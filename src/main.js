07040ff465fc961af3ae70fb10eebcec18326ae8
John H <john@real-currents.com>
2025-03-18 22:33:01 -0400

    try update render state in xr session

--- src/main.js
+++ src/main.js
@@ -19,14 +19,6 @@ let waiting_for_confirmation = false;
 
 async function initScene (setup = (scene, camera, controllers, players) => {}) {
 
-    const clock = new THREE.Clock();
-    const scene = new THREE.Scene();
-    const controllerModelFactory = new XRControllerModelFactory();
-    const controllers = {
-        left: null,
-        right: null,
-    };
-
     // iwer setup
     let nativeWebXRSupport = false;
 
@@ -72,6 +64,22 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
 
     console.log(container);
 
+    const canvas = window.document.createElement('canvas');
+
+    const clock = new THREE.Clock();
+    const scene = new THREE.Scene();
+    const controllerModelFactory = new XRControllerModelFactory();
+    const controllers = {
+        left: null,
+        right: null,
+    };
+
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(previewWindow.width, previewWindow.height);
+    renderer.xr.enabled = true;
+    container.appendChild(renderer.domElement);
+
     // Setup Stats
     const stats = new Stats();
     stats.showPanel(0);
@@ -89,14 +97,6 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
 
     scene.add(statsMesh);
 
-    const canvas= window.document.createElement('canvas');
-
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(previewWindow.width, previewWindow.height);
-    renderer.xr.enabled = true;
-    container.appendChild(renderer.domElement);
-
     const camera = new THREE.PerspectiveCamera(
         50,
         previewWindow.width / previewWindow.height,
@@ -104,14 +104,6 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
         1000,
     );
     camera.position.set(0, 1.6, 3);
-
-    const controls = new OrbitControls(camera, container);
-    controls.target.set(0, 1.6, 0);
-    controls.update();
-
-    console.log(renderer.domElement);
-
-    container.appendChild(renderer.domElement);
 
     function onWindowResize() {
         camera.aspect = previewWindow.width / previewWindow.height;
@@ -121,6 +113,10 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
     }
 
     window.addEventListener('resize', onWindowResize);
+
+    const controls = new OrbitControls(camera, container);
+    controls.target.set(0, 1.6, 0);
+    controls.update();
 
     const environment = new RoomEnvironment(renderer);
     const pmremGenerator = new THREE.PMREMGenerator(renderer);
@@ -161,187 +157,205 @@ async function initScene (setup = (scene, camera, controllers, players) => {}) {
         // gripSpace.visible = false;
     }
 
-    function startXR() {
-        const sessionInit = {
-            optionalFeatures: [
-                "local-floor",
-                "bounded-floor",
-                "hand-tracking",
-                "layers"
-            ],
-            requiredFeatures: [
-                // "webgpu"
-            ]
-        };
+    await loadManager.addLoadHandler(async () => {
 
-        navigator.xr
-            .requestSession("immersive-ar", sessionInit)
-            .then(onSessionStarted);
 
-        const vrDisplays = [];
 
-        if (navigator.getVRDisplays) {
-            function updateDisplay() {
-                // Call `navigator.getVRDisplays` (before Firefox 59).
-                navigator.getVRDisplays().then(displays => {
-                    constole.log("Checking VR display");
-                    if (!displays.length) {
-                        throw new Error('No VR display found');
-                    } else {
-                        for (const display of displays) {
-                            console.log("Found VR Display:", display);
-                            vrDisplays.push(display);
-                            container.innerHTML += `<br />
+        async function onSessionStarted(xrSession) {
+
+            const delta = clock.getDelta();
+            const time = clock.getElapsedTime();
+
+            const gl = renderer.getContext();
+
+            xrSession.addEventListener("end", onSessionEnded);
+
+            await renderer.xr.setSession(xrSession);
+
+            currentSession = xrSession;
+
+            const vrDisplays = [];
+
+            if (navigator.getVRDisplays) {
+                function updateDisplay() {
+                    // Call `navigator.getVRDisplays` (before Firefox 59).
+                    navigator.getVRDisplays().then(displays => {
+                        constole.log("Checking VR display");
+                        if (!displays.length) {
+                            throw new Error('No VR display found');
+                        } else {
+                            for (const display of displays) {
+                                console.log("Found VR Display:", display);
+                                vrDisplays.push(display);
+                                container.innerHTML += `<br />
 <span style="color: greenyellow">VR Display Connected!</span> <br />
 <span style="color: greenyellow">Reload page to reset XR scene.</span>
 `;
-                        }
-                    }
-                });
-            }
-
-            // As of Firefox 59, it's preferred to also wait for the `vrdisplayconnect` event to fire.
-            window.addEventListener('vrdisplayconnect', updateDisplay);
-            window.addEventListener('vrdisplaydisconnect', e => console.log.bind(console));
-            window.addEventListener('vrdisplayactivate', e => console.log.bind(console));
-            window.addEventListener('vrdisplaydeactivate', e => console.log.bind(console));
-            window.addEventListener('vrdisplayblur', e => console.log.bind(console));
-            window.addEventListener('vrdisplayfocus', e => console.log.bind(console));
-            window.addEventListener('vrdisplaypointerrestricted', e => console.log.bind(console));
-            window.addEventListener('vrdisplaypointerunrestricted', e => console.log.bind(console));
-            window.addEventListener('vrdisplaypresentchange', e => console.log.bind(console))
-        }
-    }
-
-    async function onSessionStarted(session) {
-        session.addEventListener("end", onSessionEnded);
-        await renderer.xr.setSession(session);
-        currentSession = session;
-    }
-
-    function onSessionEnded() {
-        currentSession.removeEventListener("end", onSessionEnded);
-        currentSession = null;
-    }
-
-    const xr_button = // VRButton.createButton(renderer);
-        document.createElement("button");
-    // xr_button.className = "vr-button";
-    xr_button.className = "xr-button";
-    xr_button.innerHTML = "Enter XR";
-    xr_button.addEventListener('click', async () => {
-
-        console.log("XR Button clicked");
-
-        const delta = clock.getDelta();
-        const time = clock.getElapsedTime();
-
-        startXR();
-
-        previewWindow.width = window.innerWidth;
-        previewWindow.height = window.innerHeight;
-
-        renderer.setSize(previewWindow.width, previewWindow.height);
-
-        camera.aspect = previewWindow.width / previewWindow.height;
-        camera.updateProjectionMatrix();
-
-        // Set camera position
-        // camera.position.z = 0;
-        camera.position.y = 0;
-
-        player.position.z = camera.position.z;
-        player.position.y = camera.position.y;
-
-        updateScene(currentSession, delta, time);
-
-        renderer.render(scene, camera);
-
-        container.style = `display: block; color: #FFF; font-size: 24px; text-align: center; background-color: #000; height: 100vh; max-width: ${previewWindow.width}px; max-height: ${previewWindow.height}px; overflow: hidden;`;
-        container.innerHTML = "Reload page";
-    });
-
-    container.append(loadManager.div);
-
-    const updateScene = await setup(scene, camera, controllers, player);
-
-    await loadManager.addLoadHandler(async () => {
-
-        // setTimeout(async () => {
-
-            renderer.setAnimationLoop(() => {
-                const delta = clock.getDelta();
-                const time = clock.getElapsedTime();
-                Object.values(controllers).forEach((controller) => {
-                    if (controller?.gamepad) {
-                        controller.gamepad.update();
-                    }
-                });
-
-                const sceneDataUpdate = {};
-
-                stats.begin();
-
-                if (controllers.hasOwnProperty("right") && controllers.right !== null) {
-
-                    const {gamepad, raySpace} = controllers.right;
-
-                    if (gamepad.getButtonClick(XR_BUTTONS.TRIGGER)) {
-                        console.log("Trigger on right controller was activated:", XR_BUTTONS.TRIGGER, gamepad);
-
-                        sceneDataUpdate.action = `Trigger on right controller was activated: ${XR_BUTTONS.TRIGGER}`;
-                        sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
-
-                    } else if (gamepad.getButtonClick(XR_BUTTONS.BUTTON_1)) {
-                        console.log("BUTTON_1 (A) on right controller was activated:", XR_BUTTONS.BUTTON_1, gamepad);
-                        if (!!waiting_for_confirmation) {
-                            console.log("Confirm action");
-                            waiting_for_confirmation = false;
-                            console.log("End session");
-                            sceneDataUpdate.action = "End session confirmed";
-                            sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
-                            currentSession.end();
-                        }
-
-                    } else if (gamepad.getButtonClick(XR_BUTTONS.BUTTON_2)) {
-                        console.log("BUTTON_2 (B) on right controller was activated:", XR_BUTTONS.BUTTON_2, gamepad);
-
-                        if (!!waiting_for_confirmation) {
-                            console.log("Cancel action");
-                            waiting_for_confirmation = false;
-                            sceneDataUpdate.action = "End session cancelled";
-                            sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
-
-                        } else {
-                            console.log("Waiting for confirmation...")
-                            waiting_for_confirmation = true;
-                            sceneDataUpdate.action = "End session initiated";
-                            sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
-                        }
-
-                    } else {
-                        for (const b in XR_BUTTONS) {
-                            if (XR_BUTTONS.hasOwnProperty(b)) {
-                                // console.log("Check button: ", XR_BUTTONS[b]);
-                                if (gamepad.getButtonClick(XR_BUTTONS[b])) {
-                                    console.log("Button on right controller was activated:", XR_BUTTONS[b], gamepad);
-                                }
                             }
                         }
-                    }
+                    });
                 }
 
-                updateScene(currentSession, delta, time, (Object.keys(sceneDataUpdate).length > 0) ? sceneDataUpdate : null);
+                // As of Firefox 59, it's preferred to also wait for the `vrdisplayconnect` event to fire.
+                window.addEventListener('vrdisplayconnect', updateDisplay);
+                window.addEventListener('vrdisplaydisconnect', e => console.log.bind(console));
+                window.addEventListener('vrdisplayactivate', e => console.log.bind(console));
+                window.addEventListener('vrdisplaydeactivate', e => console.log.bind(console));
+                window.addEventListener('vrdisplayblur', e => console.log.bind(console));
+                window.addEventListener('vrdisplayfocus', e => console.log.bind(console));
+                window.addEventListener('vrdisplaypointerrestricted', e => console.log.bind(console));
+                window.addEventListener('vrdisplaypointerunrestricted', e => console.log.bind(console));
+                window.addEventListener('vrdisplaypresentchange', e => console.log.bind(console))
+            }
 
-                renderer.render(scene, camera);
+            previewWindow.width = window.innerWidth;
+            previewWindow.height = window.innerHeight;
 
-                stats.end();
-                statsMesh.material.map.update();
-            });
+            renderer.setSize(previewWindow.width, previewWindow.height);
 
-            container.appendChild(xr_button);
+            camera.aspect = previewWindow.width / previewWindow.height;
+            camera.updateProjectionMatrix();
 
-        // }, 5333);
+            // Set camera position
+            // camera.position.z = 0;
+            camera.position.y = 0;
+
+            player.position.z = camera.position.z;
+            player.position.y = camera.position.y;
+
+            // loadWebGLResources(renderer, scene, xrSession);
+
+            // if (typeof XRWebGLBinding === "function") {
+            //
+            //     const xrGlBinding = new XRWebGLBinding(xrSession, gl);
+            //     const projectionLayer = new XRWebGLLayer(xrSession, gl);
+            //     const quadLayer = xrGlBinding.createQuadLayer({
+            //         pixelWidth: 1024,
+            //         pixelHeight: 1024,
+            //     });
+            //
+            //     xrSession.updateRenderState({
+            //         layers: [ projectionLayer, quadLayer ],
+            //     });
+            //
+            // } else {
+
+                xrSession.updateRenderState({
+                    baseLayer: new XRWebGLLayer(xrSession, gl),
+                });
+
+            // }
+
+            // const updateScene = await setup(scene, camera, controllers, player);
+            //
+            // renderer.setAnimationLoop(() => {
+            //     const delta = clock.getDelta();
+            //     const time = clock.getElapsedTime();
+            //     Object.values(controllers).forEach((controller) => {
+            //         if (controller?.gamepad) {
+            //             controller.gamepad.update();
+            //         }
+            //     });
+            //
+            //     const sceneDataUpdate = {};
+            //
+            //     stats.begin();
+            //
+            //     if (controllers.hasOwnProperty("right") && controllers.right !== null) {
+            //
+            //         const {gamepad, raySpace} = controllers.right;
+            //
+            //         if (gamepad.getButtonClick(XR_BUTTONS.TRIGGER)) {
+            //             console.log("Trigger on right controller was activated:", XR_BUTTONS.TRIGGER, gamepad);
+            //
+            //             sceneDataUpdate.action = `Trigger on right controller was activated: ${XR_BUTTONS.TRIGGER}`;
+            //             sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
+            //
+            //         } else if (gamepad.getButtonClick(XR_BUTTONS.BUTTON_1)) {
+            //             console.log("BUTTON_1 (A) on right controller was activated:", XR_BUTTONS.BUTTON_1, gamepad);
+            //             if (!!waiting_for_confirmation) {
+            //                 console.log("Confirm action");
+            //                 waiting_for_confirmation = false;
+            //                 console.log("End session");
+            //                 sceneDataUpdate.action = "End session confirmed";
+            //                 sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
+            //                 currentSession.end();
+            //             }
+            //
+            //         } else if (gamepad.getButtonClick(XR_BUTTONS.BUTTON_2)) {
+            //             console.log("BUTTON_2 (B) on right controller was activated:", XR_BUTTONS.BUTTON_2, gamepad);
+            //
+            //             if (!!waiting_for_confirmation) {
+            //                 console.log("Cancel action");
+            //                 waiting_for_confirmation = false;
+            //                 sceneDataUpdate.action = "End session cancelled";
+            //                 sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
+            //
+            //             } else {
+            //                 console.log("Waiting for confirmation...")
+            //                 waiting_for_confirmation = true;
+            //                 sceneDataUpdate.action = "End session initiated";
+            //                 sceneDataUpdate.waiting_for_confirmation = waiting_for_confirmation;
+            //             }
+            //
+            //         } else {
+            //             for (const b in XR_BUTTONS) {
+            //                 if (XR_BUTTONS.hasOwnProperty(b)) {
+            //                     // console.log("Check button: ", XR_BUTTONS[b]);
+            //                     if (gamepad.getButtonClick(XR_BUTTONS[b])) {
+            //                         console.log("Button on right controller was activated:", XR_BUTTONS[b], gamepad);
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            //
+            //     if (typeof updateScene === "function") {
+            //         updateScene(currentSession, delta, time, (Object.keys(sceneDataUpdate).length > 0) ? sceneDataUpdate : null);
+            //     }
+            //
+            //     renderer.render(scene, camera);
+            //
+            //     stats.end();
+            //     statsMesh.material.map.update();
+            // });
+
+            // container.style = `display: block; color: #FFF; font-size: 24px; text-align: center; background-color: #000; height: 100vh; max-width: ${previewWindow.width}px; max-height: ${previewWindow.height}px; overflow: hidden;`;
+            // container.innerHTML = "Reload page";
+        }
+
+        function onSessionEnded() {
+            currentSession.removeEventListener("end", onSessionEnded);
+            currentSession = null;
+        }
+
+        const xr_button = // VRButton.createButton(renderer);
+            document.createElement("button");
+        // xr_button.className = "vr-button";
+        xr_button.className = "xr-button";
+        xr_button.innerHTML = "Enter XR";
+        xr_button.addEventListener('click', async () => {
+
+            console.log("XR Button clicked");
+
+            const sessionInit = {
+                optionalFeatures: [
+                    "bounded-floor",
+                    "hand-tracking",
+                    "layers",
+                    "local-floor"
+                ],
+                requiredFeatures: [
+                    // "webgpu"
+                ]
+            };
+
+            navigator.xr
+                .requestSession("immersive-ar", sessionInit)
+                .then(onSessionStarted);
+        });
+
+        container.appendChild(xr_button);
     });
 
 }
